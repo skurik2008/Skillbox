80470ce152a20304affd14da9f52882ca0a6963a
Andrei Skuratau <skurik2008@mail.ru>
2022-03-15 20:54:18 +0300

    'initial'

---
+++ calculator2.py
@@ -0,0 +1,25 @@
+﻿action = input('Выберите операцию: ')
+while action != '+' and action != '-' and action != '*' and action != '/':
+  print('Ошибка: такой операции не существует. Попробуйте еще раз.')
+  action = input('Выберите операцию: ')
+oper = int(input('Сколько операндов? '))
+num_print = ''
+for i in range(1, oper + 1):
+  num = int(input(f"Введите операнд {i}: "))
+  if i == 1:
+    num_res = num
+    num_print += str(num)
+  else:
+    if action == '+':
+      num_res += num
+      num_print += action + str(num)
+    elif action == '-':
+      num_res -= num
+      num_print += action + str(num)
+    elif action == '*':
+      num_res *= num
+      num_print += action + str(num)
+    else:
+      num_res /= num
+      num_print += action + str(num)
+print(num_print, '=', num_res)
